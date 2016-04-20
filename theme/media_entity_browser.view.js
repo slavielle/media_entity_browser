a942952d5778aa816462109eb894b42199916b2b
rikki_iki <rikki.bochow@gmail.com>
2016-04-20 17:40:49 +1000

    Initial design improvements.

--- theme/media_entity_browser.view.js
+++ theme/media_entity_browser.view.js
@@ -1,3 +1,25 @@
-(function($) {
-  // alert('Loaded JS');
-})(jQuery);
+/**
+ * @file media_entity_browser.view.js
+ */
+(function ($) {
+
+  "use strict";
+
+  /**
+   * Registers behaviours related to view widget.
+   */
+  Drupal.behaviors.MediaEntityBrowserView = {
+    attach: function (context) {
+      $('.views-row').once('bind-click-event').click(function () {
+        var input = $(this).find('.views-field-entity-browser-select input');
+        input.prop('checked', !input.prop('checked'));
+        if (input.prop('checked')) {
+          $(this).addClass('checked');
+        }
+        else {
+          $(this).removeClass('checked');
+        }
+      });
+    }
+  }
+}(jQuery, Drupal));
